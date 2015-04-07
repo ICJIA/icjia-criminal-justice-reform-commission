39d7d2b4ad1644c0b0583f1fab15dd157e0686d9
cschweda <cschweda@gmail.com>
2015-04-07 15:30:26 -0500

    meetings and materials
    + added isotope.js to ‘meetings and materials’

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -20,20 +20,6 @@
 // 	});
 // });
 
-if ($("html").hasClass("oldie")) {
-  $('#progressBar').hide();
-};
-
-$('.expander').readmore({
-  speed: 175,
-  collapsedHeight: 100,
-  moreLink: '<a href="#"><span class="expand-style">More&nbsp;<i class="fa fa-angle-double-right"></i></span></a>',
-  lessLink: '<a href="#"><span class="expand-style"><i class="fa fa-angle-double-left"></i>&nbsp;</span></a>',
-  afterToggle: function() {
-    progressBarInit();
-  }
-});
-
 
 
 /* Custom function for Facebook sharewindow version 1.0
@@ -51,20 +37,20 @@ function fbShare(url, title, descr, winWidth, winHeight) {
 /* Custom function for Facebook sharewindow version 2.0 */
 
 function fbs_click(u, t) {
-  //u = location.href;
-  //t = document.title;
-  window.open('http://www.facebook.com/sharer.php?u=' + encodeURIComponent(u) +
-    '&t=' + encodeURIComponent(t), 'sharer',
-    'toolbar=0,status=0,width=626,height=436');
-  return false;
+	//u = location.href;
+	//t = document.title;
+	window.open('http://www.facebook.com/sharer.php?u=' + encodeURIComponent(u) +
+		'&t=' + encodeURIComponent(t), 'sharer',
+		'toolbar=0,status=0,width=626,height=436');
+	return false;
 }
 
 function twitter_click(t) {
 
-  window.open('http://twitter.com/share?text=' + encodeURIComponent(t),
-    'sharer',
-    'toolbar=0,status=0,width=626,height=436');
-  return false;
+	window.open('http://twitter.com/share?text=' + encodeURIComponent(t),
+		'sharer',
+		'toolbar=0,status=0,width=626,height=436');
+	return false;
 }
 
 
@@ -74,27 +60,70 @@ function twitter_click(t) {
  * returns version of IE or false, if browser is not Internet Explorer
  */
 function detectIE() {
-  var ua = window.navigator.userAgent;
+	var ua = window.navigator.userAgent;
 
-  var msie = ua.indexOf('MSIE ');
-  if (msie > 0) {
-    // IE 10 or older => return version number
-    return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
-  }
+	var msie = ua.indexOf('MSIE ');
+	if (msie > 0) {
+		// IE 10 or older => return version number
+		return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+	}
 
-  var trident = ua.indexOf('Trident/');
-  if (trident > 0) {
-    // IE 11 => return version number
-    var rv = ua.indexOf('rv:');
-    return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
-  }
+	var trident = ua.indexOf('Trident/');
+	if (trident > 0) {
+		// IE 11 => return version number
+		var rv = ua.indexOf('rv:');
+		return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+	}
 
-  var edge = ua.indexOf('Edge/');
-  if (edge > 0) {
-    // IE 12 => return version number
-    return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
-  }
+	var edge = ua.indexOf('Edge/');
+	if (edge > 0) {
+		// IE 12 => return version number
+		return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+	}
 
-  // other browser
-  return false;
+	// other browser
+	return false;
 }
+
+
+
+$(document).ready(function() {
+
+	var $container = $('#news-container');
+	// init
+	$container.isotope({
+		// options
+		itemSelector: '.news-item',
+		layoutMode: 'fitRows'
+	});
+
+
+
+	// filter items on button click
+	$('#filters').on('click', 'button', function() {
+		$('button').removeClass('active');
+		var filterValue = $(this).attr('data-filter');
+		$container.isotope({
+			filter: filterValue
+		});
+		$(this).addClass('active');
+	});
+
+
+
+	if ($("html").hasClass("oldie")) {
+		$('#progressBar').hide();
+	};
+
+	$('.expander').readmore({
+		speed: 175,
+		collapsedHeight: 100,
+		moreLink: '<a href="#"><span class="expand-style">More&nbsp;<i class="fa fa-angle-double-right"></i></span></a>',
+		lessLink: '<a href="#"><span class="expand-style"><i class="fa fa-angle-double-left"></i>&nbsp;</span></a>',
+		afterToggle: function() {
+			progressBarInit();
+		}
+	});
+
+
+}); // document.ready
